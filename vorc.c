7e7440e1aa6b7fabe687c06ff25ab1bdd1dbde87
ip-ilamparithi <ilamparithi@outlook.com>
2016-12-15 21:25:24 +0530

    Create vorc.c

---
+++ vorc.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+char a;
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c==''||c=='o'||c==''||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+printf("VOWEL");
+else
+printf("consonant");
+return 0;
+}
